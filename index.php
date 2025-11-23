5d32e15844263755551d7ba5c8a009fb54f9e76a
Ffffiann <alfianhakim2204@gmail.com>
2025-11-23 12:20:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
-<?php
-include 'config.php';
-$result = $conn->query("SELECT * FROM users");
-?>
+<?php include 'config.php'; ?>
 <!DOCTYPE html>
 <html>
-<head><title>CRUD Azure</title></head>
+<head>
+    <title>CRUD Azure</title>
+</head>
 <body>
-<h2>Data Users</h2>
-<a href='add.php'>Tambah Data</a><br><br>
-<table border='1' cellpadding='8'>
-<tr><th>ID</th><th>Nama</th><th>Email</th><th>Aksi</th></tr>
-<?php while($row = $result->fetch_assoc()): ?>
+<h2>Daftar Mahasiswa</h2>
+<a href="add.php">Tambah Data</a>
+<br><br>
+
+<table border="1" cellpadding="8">
 <tr>
-<td><?= $row['id'] ?></td>
-<td><?= $row['name'] ?></td>
-<td><?= $row['email'] ?></td>
-<td>
-<a href='edit.php?id=<?= $row['id'] ?>'>Edit</a> |
-<a href='delete.php?id=<?= $row['id'] ?>'>Delete</a>
-</td>
+    <th>ID</th>
+    <th>Nama</th>
+    <th>Action</th>
 </tr>
-<?php endwhile; ?>
+
+<?php
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>
+        <td>".$row['id']."</td>
+        <td>".$row['nama']."</td>
+        <td>
+            <a href='edit.php?id=".$row['id']."'>Edit</a> |
+            <a href='delete.php?id=".$row['id']."'>Delete</a>
+        </td>
+    </tr>";
+}
+?>
 </table>
+
 </body>
 </html>
